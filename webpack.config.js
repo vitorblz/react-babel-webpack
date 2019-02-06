02c89386707c6edf6753613f950c1dbac7bd9ba3
Vitor Carvalho <vitorblz@gmail.com>
2019-02-06 19:50:01 -0200

    Created a file for App component

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const path = require('path');
 module.exports = {
   entry: [
     '@babel/polyfill', 
-    './lib/components/index.js'
-  ],
+    './lib/components/index.js'],
   output: {
     path: path.resolve(__dirname,'public'),
     filename: 'bundle.js'
